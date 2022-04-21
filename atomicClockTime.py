385174258d1344cfb7760b2418673ed4d6193348
t4h1r <93381402+t4h1r@users.noreply.github.com>
2022-04-21 12:19:54 +0100

    Add files via upload

---
+++ atomicClockTime.py
@@ -0,0 +1,17 @@
+import ntplib
+from time import ctime
+from time import sleep
+# c = ntplib.NTPClient()
+# response = c.request('europe.pool.ntp.org', version=3)
+# ctime(response.tx_time)
+
+counter = 0
+
+while counter < 1 :
+    c = ntplib.NTPClient()
+    response = c.request('europe.pool.ntp.org', version=3)
+    ctime(response.tx_time)
+    print(ctime(response.tx_time))
+    sleep(1)
+
+
